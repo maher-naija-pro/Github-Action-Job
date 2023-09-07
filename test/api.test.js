2726c2284fa816124b1a53476cb96c3ee6be571d
maher <mnstudios@hotmail.com>
2023-09-07 15:47:13 +0200

    undo test

--- test/api.test.js
+++ test/api.test.js
@@ -1,22 +1,5 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const app = require('../app'); // Update the path to your Node.js API file
-const should = chai.should();
-
-chai.use(chaiHttp);
-
-describe('Hello World API', () => {
-  it('should return "Hello, World!" when accessing the root URL', (done) => {
-    chai.request(app)
-      .get('/')
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.body.should.be.a('object');
-        res.text.should.equal('Hello, World!');
-        done();
-      });
-  });
-});
 
 
 
